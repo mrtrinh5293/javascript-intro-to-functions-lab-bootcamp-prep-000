b2741ff821fa3e80533ea67ac66343fd23be73a2
Duc Dang <mrtrinh5293@gmail.com>
2018-04-25 17:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ function sayHiToGrandma(string){
   if (string === string.toUpperCase()){
     
   return "YES INDEED!"
-  if ( string = "")
+  if ( string = "I love you, Grandma."){
+    return "I love you, too."
+  }
   }
 }
 
