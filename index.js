e3cf699983ef4c9e0156f39f114ef17950273a32
Duc Dang <mrtrinh5293@gmail.com>
2018-04-25 17:29:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   return "I can\'t hear you!"
   }
   
-  if (string === strtoUpperCase()){
+  if (string === string.toUpperCase()){
     
   return "YES INDEED!"
   }
